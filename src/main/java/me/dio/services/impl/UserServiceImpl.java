40974216af29d447fc7b39d557c15a9a5b6767c2
WalSousa <walkiriasousa26@hotmail.com>
2024-10-20 00:27:28 -0300

    finalizado desenvolvimento

---
+++ src/main/java/me/dio/services/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package me.dio.services.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User findById(Long id){
+        try {
+            return userRepository.findById(id).orElseThrow();
+        }
+        catch (Exception throwable){
+            throw new NoSuchElementException(throwable.getMessage());
+        }
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
+            throw new IllegalArgumentException("this user Account number already exists");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
