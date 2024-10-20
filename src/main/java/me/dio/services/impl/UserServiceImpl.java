a368fc24d250650ee0f42ea0616003b84125428e
WalSousa <walkiriasousa26@hotmail.com>
2024-10-20 17:23:36 -0200

    ajuste cors

--- src/main/java/me/dio/services/impl/UserServiceImpl.java
+++ src/main/java/me/dio/services/impl/UserServiceImpl.java
@@ -9,10 +9,8 @@ import me.dio.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
